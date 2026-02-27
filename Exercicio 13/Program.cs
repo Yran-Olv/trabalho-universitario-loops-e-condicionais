3e8b4dc9843f165bd91c53a8cf055a8d3d98d808
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:13:58 -0300

    Atividade13

---
+++ Exercicio 13/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Atividade13
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero 1");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero 2");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a operação ( +, -, * , / ) ");
+            char op = char.Parse(Console.ReadLine());
+
+
+			if (op == '+')
+            {
+                double soma = n1 + n2;
+                Console.WriteLine("Soma = " + soma);
+            }
+            else if (op == '-')
+            {
+                double subtracao = n1 - n2;
+                Console.WriteLine("Subtração = " + subtracao);
+            }
+            else if (op == '*')
+            {
+                double multiplicacao = n1 * n2;
+                Console.WriteLine("Multiplicação = " + multiplicacao);
+            }
+            else if (op == '/')
+            {
+                double divisao = n1 / n2;
+                Console.WriteLine("Divisão = " + divisao);
+               
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida");
+			}
+
+			
+		}
+    }
+}
