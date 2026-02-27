09022ef07ab3bdc5aaaf2a32d8b5a1d4fffe8e51
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:14:12 -0300

    Atividade11

---
+++ Exercicio 11/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Atividade11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite algum numero");
+            double n = double.Parse(Console.ReadLine());
+
+            if (n > 0)
+            {
+                Console.WriteLine($"{n} é positivo");
+            }
+            else if ( n < 0)
+            {
+                Console.WriteLine($"{n} é negativo");
+            }
+			else
+			{
+                Console.WriteLine($"{n} é zero");
+            }
+		}
+    }
+}
