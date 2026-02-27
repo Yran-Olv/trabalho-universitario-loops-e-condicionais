73895ccc870dbc3b2dafb82708199b259e0235f3
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:11:34 -0300

    Atividade2

---
+++ Exercicio 2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Atividade2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Digite o valor de X1");
+			double x1 = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Digite o valor de Y1");
+			double y1 = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Digite o valor de X2");
+			double x2 = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Digite o valor de Y2");
+			double y2 = double.Parse(Console.ReadLine());
+
+			double D1 = x1 - y1;
+
+			double D2 = x2 - y2;
+
+			double D = Math.Sqrt((D1 * D1) + (D2 * D2));
+
+			Console.WriteLine("A Distancia será: " + D);
+			
+		}
+	}
+}
