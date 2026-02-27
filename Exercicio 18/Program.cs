1be26416c33199f6e93fc35aec4362305e62c14c
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:13:07 -0300

    Atividade18

---
+++ Exercicio 18/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Atividade18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a idade 1");
+            int idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a idade 2");
+            int idade2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a idade 3");
+            int idade3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a idade 4");
+            int iidade4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a idade 5");
+            int idade5 = int.Parse(Console.ReadLine());
+
+            double media = (double) (idade1 + idade2 + idade3 + iidade4 + idade5) / 5;
+
+            Console.WriteLine($"A media é = {media}");
+
+		}
+    }
+}
