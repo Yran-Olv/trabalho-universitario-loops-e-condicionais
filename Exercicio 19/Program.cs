7efd02bec5778c9c7216a2363498a74d413aace3
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:12:57 -0300

    Atividade19

---
+++ Exercicio 19/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Atividade19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int numero, i;
+			bool primo = true;
+
+			Console.Write("Digite um número: ");
+			numero = int.Parse(Console.ReadLine());
+
+			if (numero <= 1)
+			{
+				primo = false;
+			}
+			else
+			{
+				i = 2;
+
+				while (i < numero)
+				{
+					if (numero % i == 0)
+					{
+						primo = false;
+						break;
+					}
+
+					i++;
+				}
+			}
+
+			if (primo)
+			{
+				Console.WriteLine("É primo");
+			}
+			else
+			{
+				Console.WriteLine("Não é primo");
+			}
+		}
+    }
+}
