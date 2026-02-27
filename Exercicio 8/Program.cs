6fccbe147d039298b42ed835b8feefa54abfbc60
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:11:50 -0300

    Atividade8

---
+++ Exercicio 8/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Atividade8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero 1");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero 2");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero 3");
+            double n3 = double.Parse(Console.ReadLine());
+
+            if (n1 > 0 && n2 > 0 && n3 > 0)
+            {
+                double s = n1 + n2 + n3;
+
+                Console.WriteLine("Soma = " + s);
+            }
+           
+        }
+    }
+}
