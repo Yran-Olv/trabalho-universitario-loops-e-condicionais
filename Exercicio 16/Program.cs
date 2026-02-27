63fdd2fcb02f007002df2d71f1c46bc01fb6c054
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:13:30 -0300

    Atividade16

---
+++ Exercicio 16/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Atividade16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int n = 1;
+			int fatorial = 1;
+			int i = 1;
+
+			Console.Write("Digite um número: ");
+			n = int.Parse(Console.ReadLine());
+
+			while (i <= n)
+			{
+				fatorial = fatorial * i;
+				i++;
+			}
+
+			Console.WriteLine("Fatorial = " + fatorial);
+		}
+    }
+}
