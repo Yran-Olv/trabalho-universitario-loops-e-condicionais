1eea467e411c35f0c63c363c44e271910e259ea8
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:13:44 -0300

    Atividade14

---
+++ Exercicio 14/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Atividade14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+			Console.WriteLine("Digite um numero");
+            int n = int.Parse(Console.ReadLine());
+
+            while (i <= n)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+			}
+		}
+    }
+}
