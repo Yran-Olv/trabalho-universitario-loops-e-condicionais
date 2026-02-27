1b970aaa698b2dfdac70eaa2a003073c8a923213
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:12:27 -0300

    Atividade4

---
+++ Exercicio 4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Atividade4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Digite a nota do primeiro bimestre: ");
+			double p1 = double.Parse(Console.ReadLine());
+
+			Console.Write("Digite a nota do segunda bimestre: ");
+			double p2 = double.Parse(Console.ReadLine());
+
+			Console.Write("Digite a nota avaliações finais: ");
+			double p3 = double.Parse(Console.ReadLine());
+
+			double m = (p1 * 2 + p2 * 3 + p3 * 5) / 10;
+
+			Console.WriteLine("Média ponderada = " + m);
+		}
+    }
+}
