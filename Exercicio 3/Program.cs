5b590853d00c24f3476128954ab3807dee2a97e4
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:12:35 -0300

    Atividade3

---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Atividade3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor de A");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor de B");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor de C");
+            int c = int.Parse(Console.ReadLine());
+
+            int r = (a + b) * (a + b);
+            int s = (b + c) * (b + c);
+            int d = r + s;
+
+            Console.WriteLine("D é igual a:  " + d);
+		}
+    }
+}
