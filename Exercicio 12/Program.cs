1bb0f0f7d38dda21f0365eb6ea12d29ee4f86440
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:14:04 -0300

    Atividade12

---
+++ Exercicio 12/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Atividade12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0;
+            int i = 1;
+            double soma = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um numero");
+                n = double.Parse(Console.ReadLine());
+
+				soma = soma + n;
+
+				i++;
+            }
+
+            while (i <= 5);
+            {
+                Console.WriteLine($"A soma dos numeros sera {soma}");
+			}
+		}
+
+    }
+}
