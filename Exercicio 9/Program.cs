a53d67e3e4655bee496c4e1853311852c5af98c2
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:11:41 -0300

    Atividade9

---
+++ Exercicio 9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Atividade9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               int i = 1;
+            int soma = 0;
+            while (i <= 100)
+            {
+                soma = soma + i;
+                i++;
+            }
+
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
