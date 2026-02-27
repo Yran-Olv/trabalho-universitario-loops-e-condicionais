3761f83142b26ea7ce134ba3ebc3ffe117ea6c8d
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:12:08 -0300

    Atividade6

---
+++ Exercicio 6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Atividade6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o Numero 1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o Numero 2");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o Numero 3");
+            int n3 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("O maior é " + n1);
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("O maior é " + n2);
+            }
+            else
+            {
+                Console.WriteLine("O maior é " + n3);
+            }
+        }
+    }
+}
