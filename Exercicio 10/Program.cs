497209bfdaad74b47edb3fb065010bf3cfb06934
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:14:25 -0300

    Atividade10

---
+++ Exercicio 10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Atividade10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int multi = 0;
+
+			Console.WriteLine("Digite um numero");
+            int n = int.Parse(Console.ReadLine());
+
+            while (i <= 10)
+            {
+                multi = n * i;
+                Console.WriteLine($"{n} x {i} = {multi}");
+				i++;
+			}
+		}
+    }
+}
