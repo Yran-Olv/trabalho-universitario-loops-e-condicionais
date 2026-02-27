ba1d24142091e0cf9d61dea559d28c698758d63e
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:11:59 -0300

    Atividade7

---
+++ Exercicio 7/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Atividade7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero 1");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero 2");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("O maior é " + numero1);
+            }
+            else if (numero2 > numero1)
+            {
+                Console.WriteLine("O maior é " + numero2);
+            }
+
+            else
+            {
+                Console.WriteLine("Numeros são iguais");
+            }
+        }
+    }
+}
