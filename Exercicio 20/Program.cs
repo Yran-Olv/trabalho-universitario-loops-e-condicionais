427eccca65328bbd22a3eb864791fcd508e560cf
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:12:49 -0300

    Atividade20

---
+++ Exercicio 20/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Atividade20
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            double n = double.Parse(Console.ReadLine());
+
+            double dobro = n * n;
+            double triplo = n * n * n;
+			double raiz = Math.Sqrt(n);
+
+            Console.WriteLine($"Dobro = {dobro}");
+            Console.WriteLine($"Triplo = {triplo}");
+            Console.WriteLine($"Raiz Quadrada = {raiz}");
+		}
+    }
+}
