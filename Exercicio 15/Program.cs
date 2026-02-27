0574e98dd204a2b9e837eb4a39aea4bc695c5f6c
Yran Augusto <72234614+Yran-Olv@users.noreply.github.com>
2026-02-27 10:13:37 -0300

    Atividade15

---
+++ Exercicio 15/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Atividade15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+			int n = 0;
+			int par = 0;
+			int impar = 0;
+
+			do
+			{
+				Console.WriteLine("Digite um numero");
+				n = int.Parse(Console.ReadLine());
+
+				if ( n % 2 == 0)
+				{
+					par++;
+				}
+				else
+				{
+					impar ++;
+				}
+
+				i++;
+			}
+
+			while (i < 10);
+			{
+				Console.WriteLine($"Quantidade de pares = {par}");
+				Console.WriteLine($"Quantidade de Impares = {impar}");
+			}
+		}
+    }
+}
